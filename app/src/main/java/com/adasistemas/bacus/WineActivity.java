6550fde9ab109ec3721f09cef3d1c922531d1b4d
Ruben Berreguero Rica <rbrica@adasistemas.com>
2016-09-15 17:31:52 +0200

    Quitar HelloWord y añadir un log al iniciar la aplicacion

--- app/src/main/java/com/adasistemas/bacus/WineActivity.java
+++ app/src/main/java/com/adasistemas/bacus/WineActivity.java
@@ -2,12 +2,16 @@ package com.adasistemas.bacus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class WineActivity extends AppCompatActivity {
+    private static  final String TAG = WineActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wine);
+
+        Log.v(TAG,"Hola Amundiom estamos en Baccus");
     }
 }
