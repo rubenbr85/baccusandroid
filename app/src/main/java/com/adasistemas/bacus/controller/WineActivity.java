775b86277542ac294bb984e5d36fd1b16c807927
Ruben Berreguero Rica <rbrica@adasistemas.com>
2016-09-19 18:10:57 +0200

    WineActivity: usar la clase Wine par cambiar la foto, hemos creado el vino Vegabal

--- app/src/main/java/com/adasistemas/bacus/controller/WineActivity.java
+++ app/src/main/java/com/adasistemas/bacus/controller/WineActivity.java
@@ -6,16 +6,20 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.adasistemas.bacus.R;
+import com.adasistemas.bacus.model.Wine;
 
 public class WineActivity extends AppCompatActivity {
     private static  final String TAG = WineActivity.class.getSimpleName();
 
     private ImageView mWineImage = null;
+    private Wine mWine = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wine);
+
+        mWine = new Wine(5,"Vegaval","Tinto",R.drawable.vendaval,"","","","Valdepeñas");
 
         //Recoger la vista con el ID imageView
         mWineImage = (ImageView) findViewById(R.id.wine_image);
@@ -23,6 +27,6 @@ public class WineActivity extends AppCompatActivity {
 
     public void changeImage(View view){
         //Asignar a la vista la imagen de recursos son ese nombre
-        mWineImage.setImageResource(R.drawable.vendaval);
+        mWineImage.setImageResource(mWine.getPhoto());
     }
 }
