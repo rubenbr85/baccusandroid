8d9915ca793fb7a3c640be9eeaa005b44920874c
Ruben Berreguero Rica <rbrica@adasistemas.com>
2016-10-19 17:45:56 +0200

    Winery Descargar y tratar el json de una URL

--- app/src/main/java/com/adasistemas/bacus/model/Winery.java
+++ app/src/main/java/com/adasistemas/bacus/model/Winery.java
@@ -2,7 +2,18 @@ package com.adasistemas.bacus.model;
 
 import com.adasistemas.bacus.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -11,16 +22,74 @@ import java.util.List;
 
 public class Winery {
 
+    private static  final String winesURL = "http://static.keepcoding.io/baccus/wines.json";
+
     //Patron singelton
     private static  Winery sIntance = null;
     private List<Wine> mWines = null;
 
     public  static Winery getIntance(){
         if (sIntance == null){
-            sIntance= new Winery();
+            try {
+                sIntance= downloadWines();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
 
         return sIntance;
+    }
+
+    private static Winery downloadWines() throws IOException, JSONException {
+        Winery winery = new Winery();
+        winery.mWines = new LinkedList<Wine>();
+
+        URLConnection conn = new URL(winesURL).openConnection();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        StringBuilder response = new StringBuilder();
+        String line = null;
+
+        while((line = reader.readLine()) != null){
+            response.append(line);
+        }
+
+        reader.close();
+
+        JSONArray wines = new JSONArray(response.toString());
+
+        for(int wineIndex = 0; wineIndex < wines.length();wineIndex++ ){
+             String  name = null;
+             String type = null;
+             int photo = 0;
+             String companyName = null;
+             String companyWeb = null;
+             String notes = null;
+             String origin = null;
+             int rating = 0; //0 to 5
+
+            JSONObject jsonWine = wines.getJSONObject(wineIndex);
+            if (jsonWine.has("name")){
+                name= jsonWine.getString("name");
+                type =  jsonWine.getString("type");
+                companyName =  jsonWine.getString("company");
+                companyWeb =  jsonWine.getString("company_web");
+                notes =  jsonWine.getString("notes");
+                rating =  jsonWine.getInt("rating");
+                origin = jsonWine.getString("origin");
+
+                Wine wine = new Wine(rating,name,type,R.drawable.vendaval,companyName,companyWeb,notes,origin);
+                JSONArray jsonGrapes = jsonWine.getJSONArray("grapes");
+                for(int grapeIndex = 0; grapeIndex < jsonGrapes.length(); grapeIndex ++){
+                    wine.addGrape(jsonGrapes.getJSONObject(grapeIndex).getString("grape"));
+                }
+
+                winery.mWines.add(wine);
+            }
+        }
+
+        return winery;
     }
 
     public Winery(){
